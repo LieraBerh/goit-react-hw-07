8c1e3c2d47ffd71ad8fdc9fffe1626c0657f989c
LieraBerh <lierrastern@gmail.com>
2024-07-02 21:59:14 +0200

    first try

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
 import "./App.css";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchContacts } from "./redux/contactOps";
+import { selectError, selectLoading } from "./redux/contactsSlice";
+import ContactForm from "./components/ContactForm/ContactForm";
+import ContactList from "./components/ContactList/ContactList";
+import SearchBox from "./components/SearchBox/SearchBox";
 
 function App() {
+  const dispatch = useDispatch();
+  const loading = useSelector(selectLoading);
+  const error = useSelector(selectError);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
   return (
     <>
-      <p>Hello</p>
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <SearchBox />
+      {loading && !error && <b>Request in progress...</b>}
+      <ContactList />
     </>
   );
 }
